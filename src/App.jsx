61904c216367f6e2c72546176cede3f35259ce70
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-06-11 19:59:28 +0200

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import './App.css'
+// import NavBar from './components/navbar/NavBar'
+import Hero from './components/hero/Hero'
+
+function App() {
+  
+
+  return (
+    <>
+      {/* <NavBar /> */}
+      <Hero />
+    </>
+  )
+}
+
+export default App
