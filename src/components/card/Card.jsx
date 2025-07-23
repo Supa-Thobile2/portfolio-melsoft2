26ce49a79c774f604e602750a2c22defbe018130
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-23 10:44:11 +0200

    added a project update for project section

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -1,21 +1,23 @@
 import React from 'react'
 import Image from '../images/sky.jfif'
+import {MdPublic, MdCode} from 'react-icons/md'
 
 
 
 const Card = () => {
   return (
    
-        <div className="flex items-center justify-center flex-col w-full mx-auto border-2 rounded-t-4xl my-4">
+        <div className="flex items-center justify-center flex-col w-75 mx-auto rounded-t-4xl my-4">
              <img src={Image} alt="astranaut"  className=' w-full rounded-t-4xl object-contain mb-3' />
-            <div>
-                <h1 className='text-4xl font-semibold tracking-wider md:text-6xl'>Title</h1>
+            <div className='py-2'>
+                <h1 className='text-2xl font-semibold tracking-wider md:text-6xl'>Recipe book</h1>
             </div>
             <div>
-                <p>Description</p>
+                <p className='text-base text-white'>Project about</p>
             </div>
-            <div>
-                <button>Button</button>
+            <div className='flex items-center justify-evenly gap-2 my-4 p-2'>
+                <button className='py-2 px-8 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdPublic/>live</button>
+                <button className='py-2 px-8 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdCode/>Code </button>
             </div>
         </div>
     
