000c0ab63f5613d094d0f62ae26189beef425026
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-23 11:07:29 +0200

    added a project update for project section

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -7,7 +7,7 @@ import {MdPublic, MdCode} from 'react-icons/md'
 const Card = () => {
   return (
    
-        <div className="flex items-center justify-center flex-col w-75 mx-auto rounded-t-4xl my-4 shadow-lg">
+        <div className="flex items-center justify-center flex-col w-75 mx-auto rounded-t-4xl my-4 shadow-lg border-slate-900">
              <img src={Image} alt="astranaut"  className=' w-full rounded-t-4xl object-contain mb-3' />
             <div className='py-2'>
                 <h1 className='text-2xl font-semibold tracking-wider md:text-6xl'>Recipe book</h1>
