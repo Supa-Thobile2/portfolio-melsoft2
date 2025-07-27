ae333b095741725d0c4cd7a9fa8be34a51c8ce61
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-27 19:57:18 +0200

    added a a project

--- src/components/about/About.jsx
+++ src/components/about/About.jsx
@@ -10,27 +10,27 @@ function About() {
   return (
 
     <>
-       <div className='rounded-3xl h-[100vh] md:flex justify-center items-center p-6 my-20'>
+       <div className='rounded-3xl h-[100vh] md:flex justify-center items-center px-2 mb-40'>
         
             <img src={Image} alt="astranaut"  className='w-[100vw] rounded-4xl object-contain mb-2' />
       
 
         <div className=' md: mx-auto text-center'>
-              <h1 className='text-lg text-white py-2 '>Thobile Nontenja</h1>
-               <div className='flex items-center justify-center gap-2 p-1'>
+              <h1 className='text-lg text-white py-1 '>Thobile Nontenja</h1>
+               <div className='flex items-center justify-center gap-2'>
                                   <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaLinkedin/></button>
                                   <button className='py-2 px-2 bg-black rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaGithub/></button>
                                   <button className='py-2 px-2 bg-red-500 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaDiscord/></button>
               
               </div>
-              <div className='p-6'>
-                  <p className='text-start text-[12px] tracking-wide leading-6 text-white md:text-center'>I wrote my first line of code in 2019—and unlike most things I tried that year, I actually stuck with it. Since then, I’ve been deep in the trenches of syntax, semicolons, and stack traces, turning coffee into code and bugs into (eventual) features.</p>
-              <p className='text-start text-[12px] tracking-wide leading-6 text-white py-2 md:text-center'>What started as a curiosity quickly evolved into a full-blown passion, and now I build clean, efficient, and occasionally magical software solutions. Whether it’s front-end finesse, back-end wizardry, or just wrestling with version control, I’m always up for a challenge—as long as it doesn’t involve Internet Explorer.</p>
+              <div className='p-2'>
+                  <p className='text-start text-[8px] tracking-wide leading-6 text-white md:text-center'>I wrote my first line of code in 2019—and unlike most things I tried that year, I actually stuck with it. Since then, I’ve been deep in the trenches of syntax, semicolons, and stack traces, turning coffee into code and bugs into (eventual) features.</p>
+              <p className='text-start text-[8px] tracking-wide leading-6 text-white py-2 md:text-center'>What started as a curiosity quickly evolved into a full-blown passion, and now I build clean, efficient, and occasionally magical software solutions. Whether it’s front-end finesse, back-end wizardry, or just wrestling with version control, I’m always up for a challenge—as long as it doesn’t involve Internet Explorer.</p>
               </div>
               
                 <div className='flex justify-evenly items-center w-full gap-2 my-3 '>
-                  <button className='flex justify-evenly items-center gap-2  py-2 px-8 bg-blue-200 rounded-3xl gap-2 outline-0 border hover:translate-y-[10px] hover:shadow-md transition duration-300'> <FaEnvelope/>Contact Me</button>
-                  <button className='flex justify-evenly items-center gap-2  py-2 px-8 bg-blue-200 rounded-3xl gap-2 hover:translate-y-[10px] hover:shadow-md transition duration-300'>  <FaFileDownload/>Hire Me</button>
+                  <button className='flex justify-evenly items-center   py-2 px-8 bg-blue-200 rounded-3xl gap-2 outline-0 border hover:translate-y-[10px] hover:shadow-md transition duration-300'> <FaEnvelope/>Contact Me</button>
+                  <button className='flex justify-evenly items-center   py-2 px-8 bg-blue-200 rounded-3xl gap-2 hover:translate-y-[10px] hover:shadow-md transition duration-300'>  <FaFileDownload/>Hire Me</button>
 
                 </div>
         </div>
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -14,7 +14,7 @@ function Hero() {
         
         <div className=' flex items-center justify-center flex-col mx-auto w-100vw h-[100vh] mt-14 p-4 text-center border-2'>
                
-                        <img src={Image}  alt='astranaut image' className='w-25 rounded-full shadow-lg mx-auto  md:mx-auto' />
+                        <img src={Image}  alt='astranaut image' className='w-25 rounded-full border-2 shadow-lg mx-auto  md:mx-auto' />
                  
                       
                       
@@ -34,7 +34,7 @@ function Hero() {
                     <span className='text-[6px] leading-4 tracking-wide  text-base  text-white italic bold md:text-center block lg:text-base'>'First rule at Melsoft club: Talk about code. The second rule of the melsoft club: You don't think about Errors'</span>
                   </div>
 
-                  <div className='flex items-center justify-evenly gap-2'>
+                  <div className='flex items-center justify-evenly gap-2 '>
                     <button className='flex justify-evenly items-center gap-2 text-[8px] py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaFileDownload size={9}/>Hire Me</button>
                     <button className='flex justify-evenly items-center gap-2 text-[8px]  py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaEnvelope size={9}/>Contact Me</button>
 
