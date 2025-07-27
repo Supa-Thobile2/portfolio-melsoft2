02f03273ca557d7f30e8f0244233fb0650d2ca53
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-27 19:29:51 +0200

    added a a project

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -12,7 +12,7 @@ function Hero() {
 
     <>
         
-        <div className=' flex items-center justify-center flex-col mx-auto w-full h-[100vh] mt-10 text-center border-b-2'>
+        <div className=' flex items-center justify-center flex-col mx-auto w-100vw h-[100vh] mt-14 text-center border-2'>
                
                         <img src={Image}  alt='astranaut image' className='w-25 rounded-full shadow-lg mx-auto  md:mx-auto' />
                  
