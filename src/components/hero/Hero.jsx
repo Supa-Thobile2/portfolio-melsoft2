984c0c13dbc3d466a1ce5781329a5c4f81512c3e
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-27 19:41:14 +0200

    added a a project

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -19,7 +19,7 @@ function Hero() {
                       
                       
                         <h1 className='text-[14px] text-white my-1 md:text-4xl lg:text-3xl '>Thobile Nontenja</h1>
-                        <span className='text-white pb-2 text-[8px] md:text-base lg:text-base'>Full Stack Developer</span>
+                        <span className='text-white pb-1 text-[8px] md:text-base lg:text-base'>Full Stack Developer</span>
                   
                   <div className='flex items-center justify-between gap-2'>
                     <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaLinkedin/></button>
@@ -27,7 +27,7 @@ function Hero() {
                     <button className='py-2 px-2 bg-red-500 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaDiscord/></button>
 
                   </div>
-                  <div className='w-full  flex items-center justify-center flex-col px-4 py-2 mx-10 my-2'>
+                  <div className='w-full  flex items-center justify-center flex-col px-4 py-2 mx-10 my-1'>
                     <p className='text-[8px] pb-2 tracking-wide leading-4 text-white  md:text-center '>Hey there! It's a me Thobile Nontenja, a software engineer based in South Africa with a passion for building innovative digital solutions.
                        </p>
                     <p className='text-[8px] pb-2 text-white tracking-wide leading-4 text-start   md:text-center lg:text-base'>Since starting my coding journey in 2019, I've developed a strong foundation in front-end development, with expertise in HTML, CSS, and JavaScript.</p>
