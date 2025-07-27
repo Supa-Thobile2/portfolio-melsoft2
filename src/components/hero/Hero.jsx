de731317324d2c77e91df0e5a21dae2263db6880
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-27 11:06:04 +0200

    added updates to hero section

--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -12,31 +12,31 @@ function Hero() {
 
     <>
         
-        <div className=' flex items-center justify-center flex-col mx-auto w-full h-full mt-12 text-center p-4 mb-4 '>
+        <div className=' flex items-center justify-center flex-col mx-auto w-full h-[100vh] mt-12 text-center px-4'>
                
-                        <img src={Image}  alt='astranaut image' className='w-25 rounded-full shadow-lg mx-auto mb-1 md:mx-auto' />
+                        <img src={Image}  alt='astranaut image' className='w-35 rounded-full shadow-lg mx-auto  md:mx-auto' />
                  
                       
                       
                         <h1 className='text-lg text-white my-1 md:text-4xl lg:text-3xl '>Thobile Nontenja</h1>
                         <span className='text-white  py-1  md:text-base lg:text-base'>Full Stack Developer</span>
                   
-                  <div className='flex items-center justify-between gap-2'>
+                  <div className='flex items-center justify-between gap-2 p-2'>
                     <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaLinkedin/></button>
                     <button className='py-2 px-2 bg-black rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaGithub/></button>
                     <button className='py-2 px-2 bg-red-500 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaDiscord/></button>
 
                   </div>
-                  <div className='container items-center px-2 my-2'>
+                  <div className='w-full items-center px-4 py-4'>
                     <p className='text-[12px] tracking-wide leading-7 text-white text-start md:text-center '>Hey there! It's a me Thobile Nontenja, a software engineer based in South Africa with a passion for building innovative digital solutions.
                        </p>
                     <p className='text-[12px] text-white tracking-wide leading-7 text-start   md:text-center lg:text-base'>Since starting my coding journey in 2019, I've developed a strong foundation in front-end development, with expertise in HTML, CSS, and JavaScript.</p>
                     <span className='text-[12px] leading-7 tracking-wide  text-base text-start text-white italic bold md:text-center block lg:text-base'>'First rule at Melsoft club: Talk about code. The second rule of the melsoft club: You don't think about Errors'</span>
                   </div>
 
-                  <div className='flex items-center justify-between gap-2 my-2 p-2'>
-                    <button className='flex justify-evenly items-center gap-2  py-2 px-8 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaFileDownload/>Hire Me</button>
-                    <button className='flex justify-evenly items-center gap-2  py-2 px-8 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaEnvelope/>Contact Me</button>
+                  <div className='flex items-center justify-between gap-2'>
+                    <button className='flex justify-evenly items-center gap-2  py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaFileDownload/>Hire Me</button>
+                    <button className='flex justify-evenly items-center gap-2  py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaEnvelope/>Contact Me</button>
 
                   </div>
                   
