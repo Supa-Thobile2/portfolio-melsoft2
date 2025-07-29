e9390eb05c4dc2c81be5ad86478aa43c8bae595e
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-29 09:44:11 +0200

    added update

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -5,16 +5,16 @@ import { FaHtml5, FaCss3, FaJs } from 'react-icons/fa'
 
 
 
-const Card = () => {
+const Card = ({description, title}) => {
   return (
    
         <div className="flex items-center justify-center flex-col w-55 mx-auto rounded-t-4xl my-4  shadow-2xl border-amber-50 p-1">
              <img src={Image} alt="astranaut"  className=' w-full rounded-t-4xl object-contain' />
             <div className=''>
-                <h1 className='text-[14px] font-semibold tracking-wider '>Recipe book</h1>
+                <h1 className='text-[14px] font-semibold tracking-wider '>{title}</h1>
             </div>
             <div>
-                <p className='text-[10px] text-white py-1'>Project about</p>
+                <p className='text-[10px] text-white py-1'>{description}</p>
                 <div className='flex items-center justify-center gap-2 py-2'>
                                                   <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaHtml5 size={9}/></button>
                                                   <button className='py-2 px-2 bg-white rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaCss3 size={9}/></button>
--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -2,6 +2,7 @@ import { useState } from 'react'
 import Image from '../images/sky.jfif'
 import { FaCode, FaGlobe } from 'react-icons/fa'
 import Card from '../card/Card'
+import { projects } from '../constants/constants'
 
 
 
@@ -16,12 +17,18 @@ function Project() {
             <p className='text-white text-[8px] text-center'>As a bootcamp student, Ive had the opportunity  to work on various projects that shocase my skills in HTML, CSS, JS, React,Vite, and Tailwindcss.</p>
         <div className=' gap-2 mx-auto md:grid grid-cols-4'>
 
-
+          {/* {projects.map((project)=>{
+               <Card
+               key={project.id}
+               title={project.title}
+               description={project.description}
+               />
+          })} */}
             
-             <Card className='justify-self-center items-center'/>
+             <Card className='justify-self-center items-center' title={'Recipe book2'}/>
              <Card className='justify-self-center items-center' />
              <Card className='justify-self-center items-center'/>
-             <Card className='justify-self-center items-center'/>
+             <Card className='justify-self-center items-center'/> 
 
           
 
