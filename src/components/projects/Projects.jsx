b7d7f52d8daf34d0a782f8fb07ea5c55c037f1a6
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-07-29 09:53:22 +0200

    added update

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -25,10 +25,10 @@ function Project() {
                />
           })} */}
             
-             <Card className='justify-self-center items-center' title={'Recipe book2'}/>
-             <Card className='justify-self-center items-center' />
-             <Card className='justify-self-center items-center'/>
-             <Card className='justify-self-center items-center'/> 
+             <Card className='justify-self-center items-center' title={'Word Scramble'} description={"Who knew words could be this fun and frustrating? This JavaScript-powered game lets users scramble and unscramble letters like linguistic gymnasts. It's a mix of logic, interactivity, and just enough chaos to keep things interesting. Warning: may cause mild addiction and spontaneous vocabulary growth."}/>
+             <Card className='justify-self-center items-center ' title={'Travel Site'} description={"My first stop on the developer journey: building a travel blog without ever leaving my desk. Armed with nothing but HTML, CSS, and a dream, I created a clean and colorful space to share adventures I haven’t taken (yet). It's like backpacking—except the only bugs I encountered were in my code."} />
+             <Card className='justify-self-center items-center' title={'Business Redesign'} description={'Imagine if a dated business website walked into a salon and said, "Make me fabulous.'}/>
+             <Card className='justify-self-center items-center' title={'Recipe Book'} description={'A full-course experience, built from scratch—no frameworks, no microwave shortcuts. This project combined the elegance of good design with the structure of a well-prepped meal. HTML set the table, CSS brought the garnish, and the result was a deliciously responsive recipe page. Bon appétit!'}/> 
 
           
 
