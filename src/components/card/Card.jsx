6c74223ca7b418cd1d3f088bd874e89b9c69a10c
Supa-Thobile2 <thobilenontenja@gmail.com>
2025-08-19 19:54:00 +0200

    updated hero

--- src/components/card/Card.jsx
+++ src/components/card/Card.jsx
@@ -8,23 +8,23 @@ import { FaHtml5, FaCss3, FaJs } from 'react-icons/fa'
 const Card = ({description, title}) => {
   return (
    
-        <div className="flex items-center justify-center flex-col w-55 mx-auto rounded-t-4xl my-4  shadow-2xl border-amber-50 p-1">
+        <div className="flex items-center justify-center flex-col w-75 mx-auto rounded-t-4xl my-4  shadow-2xl border-amber-50 p-1">
              <img src={Image} alt="astranaut"  className=' w-full rounded-t-4xl object-contain' />
-            <div className=''>
-                <h1 className='text-[14px] font-semibold tracking-wider '>{title}</h1>
+            <div className='my-2'>
+                <h1 className='text-[24px] font-semibold tracking-wider '>{title}</h1>
             </div>
-            <div>
-                <p className='text-[10px] text-white py-1'>{description}</p>
-                <div className='flex items-center justify-center gap-2 py-2'>
-                                                  <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaHtml5 size={9}/></button>
-                                                  <button className='py-2 px-2 bg-white rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaCss3 size={9}/></button>
-                                                  <button className='py-2 px-2 bg-red-500 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaJs size={9}/></button>
+            <div className='mx-4'>
+                <p className='text-[12px] text-white py-1'>{description}</p>
+                <div className='flex items-center justify-center gap-4 py-2'>
+                                                  <button className='py-2 px-2  bg-blue-600 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaHtml5 size={11}/></button>
+                                                  <button className='py-2 px-2 bg-white rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaCss3 size={11}/></button>
+                                                  <button className='py-2 px-2 bg-red-500 rounded-full hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaJs size={11}/></button>
                               
                 </div>
             </div>
-            <div className='flex items-center justify-evenly gap-2 my-2'>
-                <button className='text-[8px] py-2 px-4 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdPublic size={9}/>live</button>
-                <button className='text-[8px] py-2 px-4 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdCode size={9}/>Code </button>
+            <div className='flex items-center justify-evenly gap-4 my-2'>
+                <button className='text-[8px] py-2 px-4 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdPublic size={11}/>live</button>
+                <button className='text-[8px] py-2 px-4 rounded-4xl bg-white flex items-center justify-evenly gap-2'><MdCode size={11}/>Code </button>
             </div>
         </div>
     
--- src/components/hero/Hero.jsx
+++ src/components/hero/Hero.jsx
@@ -28,13 +28,13 @@ function Hero() {
 
                   </div>
                   <div className='w-full  flex items-center justify-center flex-col px-4 py-2 mx-10 my-1'>
-                    <p className='text-[11px] pb-1 tracking-wide leading-4 text-white  md:text-center '>Hey there! It's a me Thobile Nontenja, a software engineer based in South Africa with a passion for building innovative digital solutions.
+                    <p className='text-[11px] pb-1 tracking-wide text-white  md:text-center '>Hey there! It's a me Thobile Nontenja, a software engineer based in South Africa with a passion for building innovative digital solutions.
                        </p>
-                    <p className='text-[11px] pb-1 text-white tracking-wide leading-4 '>Since starting my coding journey in 2019, I've developed a strong foundation in front-end development, with expertise in HTML, CSS, and JavaScript.</p>
-                    <span className='text-[10px] leading-4 tracking-wide text-white italic bold'>'First rule at Melsoft club: Talk about code. The second rule of the melsoft club: You don't think about Errors'</span>
+                    <p className='text-[11px] pb-1 text-white tracking-wide'>Since starting my coding journey in 2019, I've developed a strong foundation in front-end development, with expertise in HTML, CSS, and JavaScript.</p>
+                    <span className='text-[10px]  tracking-wide text-white italic bold'>'First rule at Melsoft club: Talk about code. The second rule of the melsoft club: You don't think about Errors'</span>
                   </div>
 
-                  <div className='flex  justify-evenly gap-2 items-center mt-4'>
+                  <div className='flex  justify-evenly gap-2 items-center mt-2'>
                     <button className='flex justify-evenly items-center gap-2 text-[8px] py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaFileDownload size={11}/>Hire Me</button>
                     <button className='flex justify-evenly items-center gap-2 text-[8px]  py-2 px-4 bg-blue-100 rounded-2xl hover:translate-y-[10px] hover:shadow-md transition duration-300'><FaEnvelope size={11}/>Contact Me</button>
 
